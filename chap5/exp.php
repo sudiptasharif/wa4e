b4689fe941f47d6ffb277950f71e0ac4e4552d65
Sharif <sudiptasharif@gmail.com>
2023-04-09 23:52:52 -0500

    Continuing Chap5

--- chap5/exp.php
+++ chap5/exp.php
@@ -13,17 +13,78 @@ l("");
 $x = "15" + 27;
 l('"15" + 27 = ' . $x);
 
+echo h3("String Concatenation");
 $a = 'Hello ' . 'World!';
 l($a);
 
-$a = 'Hello ' . '28';
+
+$a = 'Hello ' . 24;
 l($a);
 
+$a = 'Hello ' . ['testKey' => 'testValue'];
+l($a);
+
+$json = '{"foo":"bar"}';
+$a = 'Hello ' . $json;
+l($a);
+
+echo  h3("Curly Braces Good Substitution for Concatenation");
+echo p("Curly braces serve good substitution for concatenation, and they are quicker to type and code looks cleaner. Remember to use double quotes (\" \") as their content is parsed by php, because in single quotes (' ') you'll get literal name of variable provided");
+
+l("This works");
+$a = '12345';
+l('$a = \'12345\'');
+l('"abcd{$a}efgh" = ' . "abcd{$a}efgh (string inside double quotes is parsed)");
+l("This does not work");
+l('\'abcd{$a}efgh\' = ' . 'abcd{$a}efgh (string inside single quotes is not parsed)');
+
+echo h3('Order of precedence: dot operator');
+l('dot operator(.) has the same precedence as + and -');
+$var = 3;
+echo "Result: ".$var+3;
+
+echo  h3("Null Coalescing Operator");
 //$animal = "bunny";
 $country = "usa";
-
 $result = $animal ?? $country;
 l('$result = ' . $result);
+
+echo h3("Side-Effect Assingnment");
+echo "These are pure contractions. Use them sparingly";
+
+echo br();
+$out = "Hello";
+$out = $out . " ";
+$out .= "World!";
+$out .= br();
+echo $out;    
+
+$count = 0;
+$count += 1;
+echo "Count: $count" . br(); 
+
+echo h3("Casting");
+l('$a = 56');
+l('$b = 12');
+$a = 56;
+$b = 12;
+$c = $a / $b;
+l('$c = $a / $b');
+l("C: $c");
+
+l('"100" + 36.25 + true');
+$d = "100" + 36.25 + true;
+l("D: $d");
+l("D2: " . (string)$d);
+
+$e = (int) 9.9 - 1;
+l("E: $e");
+
+//$f = "sam" + 25;
+//l("F: $f");
+
+$g = "sam" . 25;
+l("G: $g");
 
 ?>
 <!DOCTYPE html>
--- phpini.php
+++
@@ -1,5 +0,0 @@
-<?php
-
-var_dump(php_ini_loaded_file(), php_ini_scanned_files());
-
-xdebug_info();
